74bc99e2c8f66e16b22dc2b602ac01010084dfb6
Tom Buchan <tbuchan1997@gmail.com>
2024-11-01 10:06:38 +0000

    Add fill functionality in js

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const smallCups = document.querySelectorAll(".cup-small");
 const litres = document.getElementById("litres");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+const reset = document.querySelector(".btn");
+
+updateBigCup();
 
 smallCups.forEach((cup, idx) => {
     cup.addEventListener("click", () =>
@@ -20,4 +23,34 @@ function highlightCups(idx) {
             cup.classList.remove("full");
         }
     })
+
+    updateBigCup();
 }
+
+function updateBigCup() {
+    const fullCups = document.querySelectorAll(".cup-small.full").length;
+    const totalCups = smallCups.length;
+
+    if(fullCups === 0) {
+        percentage.style.visibility = "hidden";
+        percentage.style.height = 0;
+    } else {
+        percentage.style.visibility = "visible";
+        percentage.style.height = `${fullCups / totalCups * 330}px`;
+        percentage.innerText = `${fullCups / totalCups * 100}%`;
+    }
+
+    if(fullCups === totalCups) {
+        remained.style.visibility = "hidden";
+        remained.style.height = 0;
+    } else {
+        remained.style.visibility = "visible";
+        litres.innerText = `${2 - (250 * fullCups / 1000)}L`;
+    }
+}
+
+reset.addEventListener("click", () => {
+    smallCups.forEach(cup => {
+        cup.classList.remove("full");
+    })
+})
